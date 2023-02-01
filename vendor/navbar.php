cb57efa860920a37fce8124d4228994811bfbdcf
dilshod71 <sultonboyevdilshod666@gmail.com>
2023-02-01 05:27:30 -1100

    soon

---
+++ vendor/navbar.php
@@ -0,0 +1,19 @@
+<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+        <h1 class="logo me-auto"><a href="../index.php">Sailor</a></h1>
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a href="../index.php" class="active">Home</a></li>
+                <li><a href="../services.php">Services</a></li>
+
+                <li><a href="../portfolio.php">Portfolio</a></li>
+                <li><a href="../pricing.php">Pricing</a></li>
+                <li><a href="../blog.php">Blog</a></li>
+
+                <li><a href="../contact.php">Contact</a></li>
+                <li><a href="../index.php" class="getstarted">Get Started</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+    </div>
+</header>
